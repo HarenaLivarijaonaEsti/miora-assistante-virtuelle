09f6a5aa9f55ced3b7bb451d0d6eb62b7274ad21
HarenaLivarijaonaEsti <liva13octobre2010@gmail.com>
2025-10-25 13:40:03 +0300

    ajout des fichiers

---
+++ landing.js
@@ -0,0 +1,10 @@
+// Ajoute un effet de défilement fluide
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
